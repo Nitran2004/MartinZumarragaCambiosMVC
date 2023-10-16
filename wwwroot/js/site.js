1094c87236c038a2a006fef413f44c0566b763e4
Nitran2004 <93067795+Nitran2004@users.noreply.github.com>
2023-10-16 09:14:50 -0500

    cambios en el JS

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,4 +1,27 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿< !-- ... (resto de tu código HTML) ... -->
 
-// Write your JavaScript code.
+< !--JavaScript adicional-- >
+<script>
+    // Función que se ejecuta cuando la página se carga
+    document.addEventListener('DOMContentLoaded', function() {
+        // Obtener la fila de la tabla de Burger (por ejemplo, la primera fila)
+        var burgerRow = document.querySelector('#burgerTable tbody tr:first-child');
+        
+        // Cambiar el color de fondo de la fila de Burger
+        if (burgerRow) {
+            burgerRow.style.backgroundColor = 'lightgreen';
+        }
+
+        // También puedes hacer cambios en el layout o Privacy aquí
+        // Por ejemplo, cambiar el color de fondo del body a azul
+        document.body.style.backgroundColor = 'lightblue';
+        
+        // O modificar el contenido del elemento con el ID 'privacyContent'
+        var privacyContent = document.getElementById('privacyContent');
+        if (privacyContent) {
+            privacyContent.textContent = 'Esta es la política de privacidad modificada mediante JavaScript.';
+        }
+    });
+</script>
+
+<!--Resto de tu código HTML-- >
