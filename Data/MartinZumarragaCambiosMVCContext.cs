6ea3a06301a5bfefb897c34cb1802addb2ed3351
Nitran2004 <93067795+Nitran2004@users.noreply.github.com>
2023-10-16 08:30:55 -0500

    Agregar archivos de proyecto.

---
+++ Data/MartinZumarragaCambiosMVCContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MartinZumarragaCambiosMVC.Models;
+
+namespace MartinZumarragaCambiosMVC.Data
+{
+    public class MartinZumarragaCambiosMVCContext : DbContext
+    {
+        public MartinZumarragaCambiosMVCContext (DbContextOptions<MartinZumarragaCambiosMVCContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MartinZumarragaCambiosMVC.Models.Burger> Burger { get; set; } = default!;
+    }
+}
